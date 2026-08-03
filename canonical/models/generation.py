a18fee93b755b6603bd0589c7a0d71cf9c91aa82
nuna-aa <adesinaanuoluwa2021@gmail.com>
2026-08-03 14:08:36 -0400

    add model generation script

---
+++ canonical/models/generation.py
@@ -0,0 +1,10 @@
+# imports
+# huggingface login
+# load models
+# set hyperparameters
+# load data
+# run forward pass for full_dataset_en
+# save responses
+# save activations layer and qkv
+# upload to huggingface
+#
